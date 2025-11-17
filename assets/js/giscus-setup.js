2072737d8117115598ed3ca0086fe3097a68a5a3
zhaoyium <116219868+zhaoyium@users.noreply.github.com>
2025-11-17 05:21:48 +0000

    Deploying to gh-pages from @ zhaoyium/zhaoyium.github.io@55991cb19b849595209cf3d7ea23c93592a87b66 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zhaoyium/zhaoyixu.github.io",
+    "data-repo": "zhaoyium/zhaoyium.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
